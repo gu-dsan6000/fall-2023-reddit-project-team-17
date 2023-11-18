775146d5b4d5f4863f34804132ff1009f1267321
victor <vad49@georgetown.edu>
2023-11-18 06:16:09 +0000

    working job

--- code/project-nlp/project-nlp-posts-and-books-processing.py
+++ code/project-nlp/project-nlp-posts-and-books-processing.py
@@ -3,6 +3,19 @@ import os
 import logging
 import argparse
 
+
+import json
+import sparknlp
+import numpy as np
+import pandas as pd
+from sparknlp.base import *
+from pyspark.ml import Pipeline
+from sparknlp.annotator import *
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+from sparknlp.pretrained import PretrainedPipeline
+
+
 # Import pyspark and build Spark session
 from pyspark.sql.functions import *
 from pyspark.sql.types import (
@@ -15,6 +28,11 @@ from pyspark.sql.types import (
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
+
+#!pip install spark-nlp
+#from sparknlp.annotator import *
+#from sparknlp.base import *
+#import sparknlp
 
 
 logging.basicConfig(format='%(asctime)s,%(levelname)s,%(module)s,%(filename)s,%(lineno)d,%(message)s', level=logging.DEBUG)
@@ -64,23 +82,96 @@ def main():
             submissions = month_df
         else:
             submissions = submissions.union(month_df)
+            
+            
 
+    submissions_small = submissions.sample(withReplacement=False, fraction=0.001, seed=42)
+
+
+    # create small dfs
+    use_small = True  # to easily swap between the small and small dfs
+    submissions_active = submissions_small if use_small else submissions
+
+
+    def clean_submissions(df: DataFrame) -> DataFrame:
+
+        # define conditions
+        conditions = (col('selftext') != "[removed]") & (col('selftext') != "[deleted]") & (col('selftext').isNotNull() & (col('selftext') != ""))
+
+
+        # apply filter
+        cleaned_df = df.filter(conditions)
+
+
+        return cleaned_df
+
+    submissions_active = clean_submissions(submissions_active)
+
+
+    # use regex the remove text after 'Edit: ' or 'edit: '
+
+    # The regular expression pattern
+    pattern = r"(?i)^(.*?)(?=Edit:|$)"
+
+    # Apply the regular expression to create a new column with the modified text
+    submissions_active = submissions_active.withColumn("selftext_modified", regexp_extract(col("selftext"), pattern, 1))
+
+
+    # define stories as posts longer than a certain length 
+    story_length = 4500
+
+    submissions_active = submissions_active.filter(length(col("selftext")) > story_length)
+
+
+    # keep only the 25% most engaging posts
+    # Calculate the approximate percentile of the 'score' column
+    quantile_value = submissions_active.approxQuantile("score", [0.85], 0.05)  # 0.05 is the relative error
+
+    # Filter the DataFrame to keep scores above or equal to this value
+    submissions_active = submissions_active.filter(col("score") >= quantile_value[0])
     
-    #submissions = spark.read.parquet(args.s3_dataset_path_submissions, header=True)
-    #submissions = spark.read.text(args.s3_dataset_path_submissions)
-    s3_path = f"s3://{args.s3_output_bucket}/{args.s3_output_prefix}/submissions"
-    logger.info(f"going to write submissions for submissions in {s3_path}")
-    submissions.write.mode("overwrite").parquet(s3_path)
     
+    # take only selftext
+    submissions_active = submissions_active.select("selftext")
     
-    
-    
+    submissions_active = submissions_active.withColumnRenamed("selftext", "text")
+
+
+    ########
+    # books
+    #######
     
     
     books = spark.read.text(args.s3_dataset_path_books)
-    s3_path = f"s3://{args.s3_output_bucket}/{args.s3_output_prefix}/books"
-    logger.info(f"going to write submissions for books in {s3_path}")
-    books.write.mode("overwrite").parquet(s3_path)
+    
+
+    
+    
+    # change col name
+    books = books.withColumnRenamed("value", "text")
+
+    
+    #s3_path = f"s3://{args.s3_output_bucket}/{args.s3_output_prefix}/books"
+    #logger.info(f"going to write submissions for books in {s3_path}")
+    #books.write.mode("overwrite").parquet(s3_path)
+    
+
+    #s3_path = f"s3://{args.s3_output_bucket}/{args.s3_output_prefix}/submissions"
+    #logger.info(f"going to write submissions for submissions in {s3_path}")
+    #submissions_active.write.mode("overwrite").parquet(s3_path)
+    
+    
+    all_model_text = books.unionByName(submissions_active)
+        
+        
+        
+        
+    s3_path = f"s3://{args.s3_output_bucket}/{args.s3_output_prefix}/all-model-text"
+    logger.info(f"going to write submissions for submissions in {s3_path}")
+    all_model_text.write.mode("overwrite").parquet(s3_path)
+    
+    
+    
 
     
 if __name__ == "__main__":
